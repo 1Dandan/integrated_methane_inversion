41a42c99a9f35aca575ff48336cc642cfbcd290c
1Dandan <dzhang@seas.harvard.edu>
2026-07-07 11:46:57 -0400

    restore Env sourcing for regridding restarts (needed on AWS)

--- src/utilities/common.sh
+++ src/utilities/common.sh
@@ -266,7 +266,7 @@ regrid_tropomi-BC-restart_gcc2gchp() {
             local regridding_method="conserve"
             ESMF_RegridWeightGen -s "$src_grid" -d "$dst_grid" -m "$regridding_method" -w "$regrid_weights_ch4" > /dev/null 2>&1
         fi
-	    # conda activate $CondaEnv
+	    conda activate $CondaEnv
         python -m gcpy.regrid_restart_file            \
 	       --stretched-grid                       \
 	       --stretch-factor "$STRETCH_FACTOR"     \
@@ -275,7 +275,7 @@ regrid_tropomi-BC-restart_gcc2gchp() {
 	       "$tropomi_bc"                          \
 	       "$regrid_weights_ch4"                  \
 	       "$template" > /dev/null 2>&1
-	    # source "$GEOSChemEnv"
+	    source "$GEOSChemEnv"
         mv new_restart_file.nc "$restart_ch4"
     else
         if [ ! -f "$dst_grid" ]; then
@@ -286,12 +286,12 @@ regrid_tropomi-BC-restart_gcc2gchp() {
             local regridding_method="conserve"
             ESMF_RegridWeightGen -s "$src_grid" -d "$dst_grid" -m "$regridding_method" -w "$regrid_weights_ch4" > /dev/null 2>&1
         fi
-        # conda activate $CondaEnv
+        conda activate $CondaEnv
         python -m gcpy.regrid_restart_file            \
                "$tropomi_bc"                          \
                "$regrid_weights_ch4"                  \
                "$template" > /dev/null 2>&1
-        # source "$GEOSChemEnv"
+        source "$GEOSChemEnv"
         mv new_restart_file.nc "$restart_ch4"
     fi
 
